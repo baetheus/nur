800e7b99f5815363013ae939c27ce0bdd27d228c
Brandon Blaylock <brandon@null.pub>
2026-04-13 16:36:09 -0700

    feat: add nameservers to headscale config

--- modules/hosts/hedy/default.nix
+++ modules/hosts/hedy/default.nix
@@ -113,6 +113,12 @@
         settings = {
           server_url = "https://net.null.pub";
           dns.base_domain = "net.null.pub";
+          dns.nameservers.global = [
+            "1.1.1.1"
+            "1.0.0.1"
+            "2606:4700:4700::1111"
+            "2606:4700:4700::1001"
+          ];
         };
       };
 
