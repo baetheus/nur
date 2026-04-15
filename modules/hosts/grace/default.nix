a2096429093d7040121634bda269f5fe17a2e000
Brandon Blaylock <brandon@null.pub>
2026-04-14 21:46:35 -0700

    feat: don't drop into niri right away

--- modules/hosts/grace/default.nix
+++ modules/hosts/grace/default.nix
@@ -51,15 +51,5 @@
     ];
 
     programs.niri.enable = true;
-
-    services.greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = "${config.programs.niri.package}/bin/niri-session";
-          user = "brandon";
-        };
-      };
-    };
   };
 }
