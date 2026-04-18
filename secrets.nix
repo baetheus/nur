cf96656cb965179f6e7f8b4b184d1ca1ca4ee9de
Brandon Blaylock <brandon@null.pub>
2026-04-17 17:09:12 -0700

    feat: rekey secrets with amelia hostkey

--- secrets.nix
+++ secrets.nix
@@ -4,29 +4,43 @@ let
   folder-b = "age1yubikey1qwqyr54w5yseu8lwusqr9tvxm8e30tv7mn3xf4swjaq6r383985k5t7fp3y";
   laptop-c = "age1yubikey1qf95j5rtmv2tunv0a4f2qecnej37zsegy840g38aq5hvzcehv2d6jqpyvzw";
 
-  admins = [ keychain-a folder-b laptop-c ];
+  admins = [
+    keychain-a
+    folder-b
+    laptop-c
+  ];
 
   # Host SSH public keys (from generated keypairs)
   hedy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQrDkQWu1OpswzEdJKcgMEevk+RAEYqNn46Qij/oNxB root@hedy";
   toph = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICsUOxhHkzo1XGriEX7Avnjez2D4GgTEDixtu2U9cp18 root@toph";
   grace = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK6B862RFK8HYt3qH4S0TANlXuc61gN5kAg9V7IbHZPp root@grace";
+  amelia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP590LO8yb/tAxeS6rnfZZ+5R4B6HBxJMeZTd89aXZrQ root@amelia";
 
-  hosts = [ hedy toph grace ];
+  hosts = [
+    hedy
+    toph
+    grace
+    amelia
+  ];
 
   all = admins ++ hosts;
 in
 {
   # Shared secrets (all NixOS hosts)
-  "modules/secrets/msmtp-passwordeval.age".publicKeys = all;
-  "modules/secrets/headscale-preauth-brandon.age".publicKeys = all;
-  "modules/secrets/brandon-password.age".publicKeys = all;
+  "secrets/msmtp-passwordeval.age".publicKeys = all;
+  "secrets/headscale-preauth-brandon.age".publicKeys = all;
+  "secrets/brandon-password.age".publicKeys = all;
 
   # Host-specific secrets
-  "modules/secrets/wifi-tuna.age".publicKeys = admins ++ [ toph grace ];
-  "modules/secrets/vaultwarden.age".publicKeys = admins ++ [ toph ];
-  "modules/secrets/restic-htpasswd.age".publicKeys = admins ++ [ toph ];
-  "modules/secrets/nzbget-conf.age".publicKeys = admins ++ [ toph ];
-  "modules/secrets/restic-env-toph-persist.age".publicKeys = admins ++ [ toph ];
-  "modules/secrets/restic-env-hedy-persist.age".publicKeys = admins ++ [ hedy ];
-  "modules/secrets/restic-env-grace-persist.age".publicKeys = admins ++ [ grace ];
+  "secrets/wifi-tuna.age".publicKeys = admins ++ [
+    toph
+    grace
+    amelia
+  ];
+  "secrets/vaultwarden.age".publicKeys = admins ++ [ toph ];
+  "secrets/restic-htpasswd.age".publicKeys = admins ++ [ toph ];
+  "secrets/nzbget-conf.age".publicKeys = admins ++ [ toph ];
+  "secrets/restic-env-toph-persist.age".publicKeys = admins ++ [ toph ];
+  "secrets/restic-env-hedy-persist.age".publicKeys = admins ++ [ hedy ];
+  "secrets/restic-env-grace-persist.age".publicKeys = admins ++ [ grace ];
 }
